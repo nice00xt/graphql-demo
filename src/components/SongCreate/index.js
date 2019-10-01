fd91be92aab2833a101896cc7773ca3ae48526b4
Juan Mejia <juan00xt@gmail.com>
2019-10-01 13:27:15 -0500

    First commit

---
+++ src/components/SongCreate/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class SongCreate extends Component {
+  render () {
+    return (
+      <form>
+        <label>Add Song</label>
+        <input type='text'/>
+      </form>
+    )
+  }
+}
+
+export default SongCreate;
