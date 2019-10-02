297439d889069127c72ba6d13d89ca82abc0d8f2
Juan Mejia <juan00xt@gmail.com>
2019-10-02 14:12:05 -0500

    Add queries

---
+++ src/queries/song.js
@@ -0,0 +1,27 @@
+import gql from 'graphql-tag'
+
+export const fetchSongs = gql`
+  {
+    songs {
+      id
+      title
+    }
+  }
+`;
+
+export const fetchSong = gql`
+  query SongQuery($id: ID!) {
+    song(id: $id) {
+      id
+      title
+    }
+  }
+`;
+
+export const deleteSong = gql`
+  mutation DeleteSong($id: ID) {
+    deleteSong(id: $id) {
+      id
+    }
+  }
+`;
